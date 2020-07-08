5bd1eee85aae5f839ea0a0d78bb7ab886792144b
Tim Fraunholz <fraunholz.t@web.de>
2020-07-08 17:18:24 +0200

    basic 1to1 comparison of curve sdl

--- string_comparison.py
+++ string_comparison.py
@@ -11,6 +11,9 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 """has to be symmetric around zero"""
+NUM_ALPHABET = 7
+
+
 class cur(Enum):
     STRONG_LEFT = -3
     LEFT = -2
@@ -27,10 +30,20 @@ class StringComparer:
         self.all_angles = []
         self.percentile_values = []
 
+        self.gather_all_angles()
+        self.get_curve_distribution()
+
         self.all_roads_to_curvature_sdl()
 
-        self.get_curve_distribution()
+        print("self.all_angles", self.all_angles)
         self.get_const_for_angle(200)
+
+        # for testing purposes
+        first_test_road = self.data_dict['1-1']
+        second_test_road = self.data_dict['1-2']
+        third_test_road = self.data_dict['1-3']
+        print("first road", first_test_road, "first road", second_test_road)
+        print("first to third road", self.cur_sdl_one_to_one(first_test_road['curve_sdl'], third_test_road['curve_sdl']))
 
     def all_roads_to_curvature_sdl(self):
         """ Performs shape definition language on all roads represented as asfault nodes
@@ -41,10 +54,26 @@ class StringComparer:
         assert self.data_dict is not None, "There have to be roads added"
         for name in self.data_dict:
             test = self.data_dict[name]
-            #print("name, test", name, test)
+            # print("name, test", name, test)
             nodes = test['nodes']
             assert nodes is not None, "There have to be nodes for each road"
-            self.nodes_to_curvature_sdl(name=name, nodes=nodes)
+            # TODO add road to dict
+            self.data_dict[name]['curve_sdl'] = self.nodes_to_curvature_sdl(name=name, nodes=nodes)
+
+
+    def gather_all_angles(self):
+        """ is needed to be able to get the percentiles
+        has an performance overhead, should be avoided
+
+        :param nodes:
+        :return:
+        """
+        for name in self.data_dict:
+            test = self.data_dict[name]
+            nodes = test['nodes']
+            assert nodes is not None, "There have to be nodes for each road"
+            for node in nodes:
+                self.all_angles.append(node.angle)
 
     def nodes_to_curvature_sdl(self, name: str, nodes: List[asfault.network.NetworkNode]):
         """ shape definition language of a single road
@@ -52,14 +81,36 @@ class StringComparer:
         :return:
         """
         curve_sdl = []
-        self.all_angles = []
+        # self.all_angles = []
         jo = 0
         for node in nodes:
-            print(node.angle)
-            self.all_angles.append(node.angle)
+            curve_sdl.append(self.get_const_for_angle(node.angle))
+            # fixme distribution seems many zeros, straights get classified as slight lefts
+            print("node.roadtype", node.roadtype, "; angle ", node.angle, "; cur type",
+                  self.get_const_for_angle(node.angle))
             if jo == 0:
                 print("print node to dict ", node.to_dict(node))
                 jo = 1
+        print("curve_sdl", curve_sdl)
+        return curve_sdl
+
+    def cur_sdl_one_to_one(self, curve1_sdl, curve2_sdl):
+        best_similarity = float('inf')
+        best_startpoint = 0
+        if len(curve1_sdl) < len(curve2_sdl):
+            shorter_road = curve1_sdl
+            longer_road = curve2_sdl
+        else:
+            shorter_road = curve2_sdl
+            longer_road = curve1_sdl
+        for start_point in range(0, len(longer_road)):
+            error = 0
+            for i in range(0, len(shorter_road)):
+                error += abs(longer_road[(start_point + i) % len(longer_road)].value - shorter_road[i].value)
+            if error < best_similarity:
+                best_similarity = error
+                best_startpoint = start_point
+        return best_similarity
 
     def get_const_for_angle(self, angle: float):
         """ returns the representation for an angle based on the percentiles
@@ -70,10 +121,11 @@ class StringComparer:
         """
         assert self.percentile_values, "percentile values have to be defined"
 
+        # start value at most negative -> most left value
         # type conversions for rounding to zero
-        cur_i = int(float(len(cur)/2))
+        cur_i = -int(float(len(cur) / 2))
+        # index for the curvature percentile array
         per_j = 1
-        print("cur_i", cur_i)
         if angle <= self.percentile_values[-1]:
             while angle > self.percentile_values[per_j]:
                 cur_i += 1
@@ -84,21 +136,22 @@ class StringComparer:
 
     def get_curve_distribution(self):
         """ draws a box plot and self.percentile_values with bounds of all angles
+        has to be called after all curves were compressed using shape definition
+        has an performance overhead, should be avoided, borders fixed
 
         :return: None
         """
         if not self.all_angles:
             self.all_roads_to_curvature_sdl()
-        #fig1, ax1 = plt.subplots()
-        #ax1.set_title('Basic Plot')
+        # fig1, ax1 = plt.subplots()
+        # ax1.set_title('Basic Plot')
         plt.boxplot(self.all_angles)
         plt.show()
 
-        num_alphabet = 7
-        percentile_step = 100/num_alphabet
+        percentile_step = 100 / NUM_ALPHABET
         percentile_step_sum = 0
         self.percentile_values = []
-        for i in range(0, num_alphabet+1):
+        for i in range(0, NUM_ALPHABET + 1):
             print("percentile_step_sum", percentile_step_sum)
             self.percentile_values.append(np.percentile(self.all_angles, percentile_step_sum))
             percentile_step_sum += percentile_step
@@ -109,9 +162,10 @@ class StringComparer:
         median = np.percentile(self.all_angles, 50)
         print("median of all roads", median)
 
-        strongest_left = np.percentile(self.all_angles, 14)
-        medium_left = np.percentile(self.all_angles, 28)
-        smallest_left = np.percentile(self.all_angles, 42)
-        strongest_right = np.percentile(self.all_angles, 86)
-        medium_right = np.percentile(self.all_angles, 82)
-        smallest_right = np.percentile(self.all_angles, 58)
+        # TODO remove the check and ensure a good balance
+        # <= 0 <= is counter productive, maybe look at the distribution of all right curves with half of the straights
+        # and all left curves with half of the straights
+        assert -2 < self.percentile_values[int(np.ceil(NUM_ALPHABET / 2))] < 2 \
+            and self.percentile_values[int(np.ceil(NUM_ALPHABET / 2)) - 1] <= 0 <= self.percentile_values[
+                   int(np.ceil(NUM_ALPHABET / 2)) + 1], \
+            "the curve distribution of the dataset is not balanced!"
