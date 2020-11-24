c48966c961253f8f2890614cb3c5907ee60a9645
Tim Fraunholz <fraunholz.t@web.de>
2020-11-24 15:00:24 +0100

    changed scatterplto appearance

--- clusterer.py
+++ clusterer.py
@@ -11,7 +11,7 @@ class Clusterer:
 
         assert len(dist_matrix) == len(dist_matrix[0]), "The distance matrix has to be a square!"
         print("dist_matrix", dist_matrix)
-        clust = OPTICS(eps=1000, metric='precomputed')
+        clust = OPTICS(eps=0.3, metric='precomputed')
         clust.fit(dist_matrix)
         print("clust.ordering_", clust.ordering_)
         print("clust.xi", clust.xi)
@@ -23,7 +23,7 @@ class Clusterer:
         #ordering, _, _, _ = compute_optics_graph(X=dist_matrix, metric="precomputed")
         #print("ordering", ordering)
 
-    def networkx_plot_measure(self, measure: str, draw_graphweights: bool = True, draw_edges: bool = False):
+    def networkx_plot_measure(self, measure: str, draw_node_names = True, draw_graphweights: bool = True, draw_edges: bool = False):
         """ Plots all specimens of the main dict. A measure is selected for the edge weights.
         Graphvis tries to get the optimal arrangement based on weights, hard to do for 2d space.
         Use dot, neato and fdp seem to maximize distances and just create a circular arrangement.
@@ -41,14 +41,22 @@ class Clusterer:
         G.graph['edges']={'arrowsize':'4.0'}
         # neato, fdp seem to mazimize --> circle shape
         # dot works best, however, 2d representation is difficult
-        pos = graphviz_layout(G, prog="dot")
+        pos = graphviz_layout(G, prog="fdp")
         if draw_edges:
             nx.draw_networkx(G, pos=pos)
         else:
             nx.draw_networkx_nodes(G, pos=pos)
         if draw_graphweights:
             nx.draw_networkx_edge_labels(G, pos=pos)
+        if draw_node_names:
+            nx.draw_networkx_labels(G, pos)
         plt.tight_layout()
+        # adjust the [x, y] values for fitting all the text
+        axes = plt.gca()
+        #axes.set_xlim([-60, 800])
+        # use these to turn off axis for plotting
+        #plt.xticks([])
+        #plt.yticks([])
         plt.show()
 
     def networkx_plot_measure_spring_equal_length_edges(self, measure: str):
